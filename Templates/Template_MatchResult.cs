60e67af23c0f1a447f4dd5b917cd6deb06e406d5
kumapapa2012 <morimorikenken29@hotmail.co.jp>
2025-09-24 23:35:52 +0900

    Template Changes: Youtube doesn't allow "<" ">"

--- Templates/Template_MatchResult.cs
+++ Templates/Template_MatchResult.cs
@@ -8,7 +8,9 @@
             Ended : {{ ended_at }}
             Duration : {{ duration }}
             Total Players: {{ total_players }}(Humans : {{ human_players }} / Bots : {{ bot_players }})
+
             {{ player_result }}
+
             {{ system_info }}
 
             (This output has been provided by: https://github.com/Kumapapa2012/Fortnite_Replay_Parser_GUI/)
@@ -16,15 +18,13 @@
 
         public static string PlayerResultTemplate = """
             ========= Player Results =========
-            {{ for elim in eliminations }}
-            {{ fn_form_number elim.index }}: {{ elim.time }} - {{ elim.player_name }}({{ if elim.is_bot }}bot{{ else }}human{{ end }}){{ end }}
-            {{ if eliminated }}
-            >>>> {{ player_name }} was eliminated by {{ eliminated.player_name }}({{ if eliminated.is_bot }}bot{{ else }}human{{ end }}) at {{ eliminated.time }} (Placement: {{ fn_form_number placement }})
-            {{ else }}
-            {{ if placement ==1 }}>>>> {{ player_name }} won the game!
-            {{ else }}>>>> The replay ended before the player was eliminated(or someone won the game).
+            Player : {{ player_name }} ({{ if is_bot }}bot{{ else }}human{{ end }}) eliminated:
+
+            {{ for elim in eliminations }}{{ fn_form_number elim.index }}: {{ elim.time }} - {{ elim.player_name }}({{ if elim.is_bot }}bot{{ else }}human{{ end }})
             {{ end }}
-            {{ end }}
+            {{ if eliminated }}{{ player_name }} was eliminated by {{ eliminated.player_name }}({{ if eliminated.is_bot }}bot{{ else }}human{{ end }}) at {{ eliminated.time }} (Placement: {{ fn_form_number placement }})
+            {{ else }}{{ if placement ==1 }} {{ player_name }} won the game!{{ else }}The replay ended before the match ends.
+            {{ end }}{{ end }}
             """;
 
         public static string SystemInfoTemplate = """
